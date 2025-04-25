e03b07da80040eedeb446bb2a618dbc35b4d6e96
Abigail Elis <abigailelis.s@hotmail.com>
2025-04-25 18:12:21 -0300

    modifica una de las dependencias

--- src/main/java/Repository/EstudianteRepository.java
+++ src/main/java/Repository/EstudianteRepository.java
@@ -1,4 +1,37 @@
 package Repository;
 
+import Entities.Estudiante;
+import javax.persistence.EntityManager;
+import java.io.IOException;
+
 public class EstudianteRepository {
+
+    /**
+     * Reads a CSV file and inserts new student records into the database.
+     * Each row in the CSV should contain student details
+     *
+     * @param urlFile the file path or URL of the CSV file to be processed.
+     * @throws IOException if an error occurs while reading the CSV file.
+     * @throws CsvValidationException if the CSV format is invalid or data validation fails.
+     */
+    public void insertarEstudianteCSV(String urlFile) throws IOException, CsvValidationException {
+        EntityManager em = JPAUtil.getEntityManager();
+        CSVReader reader = new CSVReader(new FileReader(urlFile));
+        String[] line;
+        reader.readNext();
+
+        em.getTransaction().begin();
+
+        while((line = reader.readNext()) != null){
+            Estudiante estudiante = new Estudiante();
+
+            estudiante.setDNI(Integer.parseInt(line[0]));
+
+            em.persist(person);
+        }
+
+        em.getTransaction().commit();
+        System.out.print("\nPeople added successfully");
+        em.close();
+    }
 }
